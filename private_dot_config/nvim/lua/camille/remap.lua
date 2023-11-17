bd63477f4f8470474e2000048367ce51b42f2e8d
Camille Vauchel <camille.vauchel@gmail.com>
2023-11-17 18:42:20 +0100

    Change harpoon & window switch

--- private_dot_config/nvim/lua/camille/remap.lua
+++ private_dot_config/nvim/lua/camille/remap.lua
@@ -23,3 +23,10 @@ end)
 vim.keymap.set("n", "<C-p>", "<cmd>cnext<CR>zz")
 
 vim.keymap.set("n", "<C-c>", "<cmd>only<CR>")
+
+
+vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>")
+
